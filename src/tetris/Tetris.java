5972da02c8b9b0102923a4bf96c4ea74bf249186
John Sanchez <jostsace@estudiantec.cr>
2021-10-08 09:20:33 -0600

    Incorporación del código inicial para el proyecto
    Se incorporaron los archivos iniciales del proyecto

---
+++ src/tetris/Tetris.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetris;
+
+/**
+ *
+ * @author John
+ */
+public class Tetris {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainForm mainWindow = new MainForm();
+        mainWindow.setVisible(true);
+    }
+    
+}
