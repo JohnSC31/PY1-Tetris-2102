44949aa7512f2b1b09ca46588526710867165b96
John Sanchez <jostsace@estudiantec.cr>
2021-10-09 13:33:26 -0600

    Representación de forma gráfica para la matriz en la clase board, ajustes en de tamaño en la ventada del juego GameForm, y creacion de un arreglo de colores con los colores para cada pieza en la clase Board

--- src/tetris/Board.java
+++ src/tetris/Board.java
@@ -5,17 +5,33 @@
  */
 package tetris;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 // esto es un nuevo cambio
-public class Board {
+public class Board extends JPanel{
     //Atributos
-    private static final int ROWS = 25;
-    private static final int COLS = 10;
-    private static final int HEIGHT = 50;
-    private static final int WIDTH = 50;
+    public static final int ROWS = 25;
+    public static final int COLS = 10;
+    public static final int HEIGHT = 30;
+    public static final int WIDTH = 30;
+    
+    // Definicion de los colores por tipo de pieza
+    private static final Color COLORS[] = {
+        new Color(0,0,0), // 0 Celda en blanco sin ningun valor
+        new Color(236,110,87), // 1 Pieza en J
+        new Color(236,208,48), // 2 Pieza en L
+        new Color(143,206,32), // 3 Pieza en O
+        new Color(17,193,155), // 4 Pieza en S
+        new Color(14,119,192), // 5 Pieza en Z
+        new Color(202,30,223), // 6 Pieza en T
+        new Color(223,30,141)}; // 7 Pieza en I
     
     private int[][] board = initBoard();
     
@@ -38,19 +54,28 @@ public class Board {
         return board;
     }
     
-    public void printBoard(JPanel panel){
-        Graphics g = panel.getGraphics();
-        int x = panel.getX();
-        int y = panel.getY();
+    public void printBoard(JPanel panelBoard){
+        int x = 0;
+        int y = 0 -((ROWS - 20) * HEIGHT); // oculta las filas de arriba para que solo se muestren 20
         
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLS; j++) {
-                g.drawRect(x, y, WIDTH, HEIGHT);
-                g.fillRect(240,52,52,1);
+                panelBoard.add(createPanel(x, y, this.board[i][j]));
                 x += WIDTH;
             }
+            x = 0;
             y += HEIGHT;
         }
+    }
+    
+    private JPanel createPanel(int x , int y, int value){
+        JPanel panel = new JPanel();
+        panel.setBackground(COLORS[value]);
+        panel.setSize(WIDTH , HEIGHT);
+        panel.setBorder(BorderFactory.createLineBorder(Color.white, 1));
+        panel.setLocation(x, y);      
+        return panel;
+    
     }
     
     // GETTERS AND SETTERS
