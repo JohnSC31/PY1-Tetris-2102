f5887697fca9ecb5d44dc94a9c515c757b36d18f
John Sanchez <jostsace@estudiantec.cr>
2021-10-13 21:35:03 -0600

    Nuevas funcionalidades
    Se incorporo el cronometro, se agregaron los scores y las lineas en el form y agrego la visualizacion de las siguientes figuras y se agregaron los key listener para las flechas y las letras AWSD

--- src/tetris/GameForm.java
+++ src/tetris/GameForm.java
@@ -6,6 +6,10 @@
 package tetris;
 
 import java.awt.Dimension;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import static java.lang.Thread.sleep;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,7 +26,7 @@ public class GameForm extends javax.swing.JFrame{
     public GameForm() {
         initComponents();
         this.setSize(new Dimension(670, 640));
-        this.game = new ThreadGame(pnlBoard);
+        this.game = new ThreadGame(pnlBoard, lblTimer, lblScore, lblLines, lblLevel, pnlNext1, pnlNext2);
         this.game.start();
     }
  
@@ -34,20 +38,21 @@ public class GameForm extends javax.swing.JFrame{
         pnlBoard = new javax.swing.JPanel();
         lblNext = new javax.swing.JLabel();
         lblScore = new javax.swing.JLabel();
+        lblLevel = new javax.swing.JLabel();
         lblLines = new javax.swing.JLabel();
-        lblLines1 = new javax.swing.JLabel();
         pnlNext1 = new javax.swing.JPanel();
         pnlNext2 = new javax.swing.JPanel();
         lblTimer = new javax.swing.JLabel();
-        btnBajar = new javax.swing.JButton();
-        btnDerecha = new javax.swing.JButton();
-        btnIzquierda = new javax.swing.JButton();
-        btnGirar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(672, 600));
         setResizable(false);
         setSize(new java.awt.Dimension(0, 600));
+        addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                formKeyReleased(evt);
+            }
+        });
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setBackground(new java.awt.Color(0, 0, 0));
@@ -65,7 +70,7 @@ public class GameForm extends javax.swing.JFrame{
         );
         pnlBoardLayout.setVerticalGroup(
             pnlBoardLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGap(0, 598, Short.MAX_VALUE)
         );
 
         jPanel1.add(pnlBoard, new org.netbeans.lib.awtextra.AbsoluteConstraints(137, 0, -1, 600));
@@ -80,49 +85,49 @@ public class GameForm extends javax.swing.JFrame{
         lblScore.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         lblScore.setForeground(new java.awt.Color(255, 255, 255));
         lblScore.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
-        lblScore.setText("Score: 12");
+        lblScore.setText("Puntos 0");
         lblScore.setToolTipText("");
         jPanel1.add(lblScore, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 106, 109, 34));
+
+        lblLevel.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        lblLevel.setForeground(new java.awt.Color(255, 255, 255));
+        lblLevel.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        lblLevel.setText("Nivel 1");
+        lblLevel.setToolTipText("");
+        jPanel1.add(lblLevel, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 230, 109, 34));
 
         lblLines.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         lblLines.setForeground(new java.awt.Color(255, 255, 255));
         lblLines.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
-        lblLines.setText("Level: 1");
+        lblLines.setText("Lineas 0");
         lblLines.setToolTipText("");
-        jPanel1.add(lblLines, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 230, 109, 34));
-
-        lblLines1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        lblLines1.setForeground(new java.awt.Color(255, 255, 255));
-        lblLines1.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
-        lblLines1.setText("Lines: 12");
-        lblLines1.setToolTipText("");
-        jPanel1.add(lblLines1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 169, 109, 34));
+        jPanel1.add(lblLines, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 169, 109, 34));
 
         javax.swing.GroupLayout pnlNext1Layout = new javax.swing.GroupLayout(pnlNext1);
         pnlNext1.setLayout(pnlNext1Layout);
         pnlNext1Layout.setHorizontalGroup(
             pnlNext1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 161, Short.MAX_VALUE)
+            .addGap(0, 150, Short.MAX_VALUE)
         );
         pnlNext1Layout.setVerticalGroup(
             pnlNext1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 142, Short.MAX_VALUE)
+            .addGap(0, 150, Short.MAX_VALUE)
         );
 
-        jPanel1.add(pnlNext1, new org.netbeans.lib.awtextra.AbsoluteConstraints(466, 64, -1, -1));
+        jPanel1.add(pnlNext1, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 70, 150, 150));
 
         javax.swing.GroupLayout pnlNext2Layout = new javax.swing.GroupLayout(pnlNext2);
         pnlNext2.setLayout(pnlNext2Layout);
         pnlNext2Layout.setHorizontalGroup(
             pnlNext2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 161, Short.MAX_VALUE)
+            .addGap(0, 150, Short.MAX_VALUE)
         );
         pnlNext2Layout.setVerticalGroup(
             pnlNext2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 150, Short.MAX_VALUE)
         );
 
-        jPanel1.add(pnlNext2, new org.netbeans.lib.awtextra.AbsoluteConstraints(466, 224, -1, -1));
+        jPanel1.add(pnlNext2, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 240, 150, 150));
 
         lblTimer.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         lblTimer.setForeground(new java.awt.Color(255, 255, 255));
@@ -131,69 +136,29 @@ public class GameForm extends javax.swing.JFrame{
         lblTimer.setToolTipText("");
         jPanel1.add(lblTimer, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 21, 93, 34));
 
-        btnBajar.setText("Bajar");
-        btnBajar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnBajarActionPerformed(evt);
-            }
-        });
-        jPanel1.add(btnBajar, new org.netbeans.lib.awtextra.AbsoluteConstraints(31, 315, -1, -1));
-
-        btnDerecha.setText("Derecha");
-        btnDerecha.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnDerechaActionPerformed(evt);
-            }
-        });
-        jPanel1.add(btnDerecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(23, 356, -1, -1));
-
-        btnIzquierda.setText("Izquierda");
-        btnIzquierda.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnIzquierdaActionPerformed(evt);
-            }
-        });
-        jPanel1.add(btnIzquierda, new org.netbeans.lib.awtextra.AbsoluteConstraints(23, 397, -1, -1));
-
-        btnGirar.setText("Girar");
-        btnGirar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnGirarActionPerformed(evt);
-            }
-        });
-        jPanel1.add(btnGirar, new org.netbeans.lib.awtextra.AbsoluteConstraints(34, 438, -1, -1));
-
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 658, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnBajarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBajarActionPerformed
-        game.board.fallFigure();
-    }//GEN-LAST:event_btnBajarActionPerformed
-
-    private void btnDerechaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDerechaActionPerformed
-        game.board.moveRight();
-        
-    }//GEN-LAST:event_btnDerechaActionPerformed
-
-    private void btnIzquierdaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIzquierdaActionPerformed
-        game.board.moveLeft();
-    }//GEN-LAST:event_btnIzquierdaActionPerformed
-
-    private void btnGirarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGirarActionPerformed
-        game.board.rotateFigure();
-    }//GEN-LAST:event_btnGirarActionPerformed
+    private void formKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyReleased
+ 
+        if(evt.getKeyCode() == KeyEvent.VK_UP || evt.getKeyCode() == KeyEvent.VK_W){
+             game.board.rotateFigure();
+        }else if(evt.getKeyCode() == KeyEvent.VK_DOWN || evt.getKeyCode() == KeyEvent.VK_S){
+            game.board.fallFigure();
+        }else if(evt.getKeyCode() == KeyEvent.VK_RIGHT || evt.getKeyCode() == KeyEvent.VK_D){
+            game.board.moveRight();
+        }else if(evt.getKeyCode() == KeyEvent.VK_LEFT || evt.getKeyCode() == KeyEvent.VK_A){
+            game.board.moveLeft();
+        }
+    }//GEN-LAST:event_formKeyReleased
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnBajar;
-    private javax.swing.JButton btnDerecha;
-    private javax.swing.JButton btnGirar;
-    private javax.swing.JButton btnIzquierda;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lblLevel;
     private javax.swing.JLabel lblLines;
-    private javax.swing.JLabel lblLines1;
     private javax.swing.JLabel lblNext;
     private javax.swing.JLabel lblScore;
     private javax.swing.JLabel lblTimer;
